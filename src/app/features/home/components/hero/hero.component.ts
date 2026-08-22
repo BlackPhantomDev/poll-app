c8a37412a719403c6e3fce0dd60cc31222faaba8
M. Siegrist <therealphantom22@proton.me>
2026-08-22 22:19:58 +0200

    feat: add create survey button with hover icon

--- src/app/features/home/components/hero/hero.component.ts
+++ src/app/features/home/components/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { Component, output } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { AddIconComponent } from '../../../../shared/components/add-icon/add-icon.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, AddIconComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
---
+++ src/app/shared/components/add-icon/add-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-icon',
+  imports: [],
+  templateUrl: './add-icon.component.html',
+  styleUrl: './add-icon.component.scss',
+})
+export class AddIconComponent {}
